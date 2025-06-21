c48858589ab5aff098b826cca8a9a4910aed380d
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2025-06-22 05:40:13 +0700

    feat: initial commit + login and register user func

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Link from "next/link"
+
+export default function HomePage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Link
+        href="/auth/login"
+        className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+      >
+        Go to App
+      </Link>
+    </div>
+  )
+}
