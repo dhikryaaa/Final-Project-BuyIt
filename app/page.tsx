cd7d90be04d6622e284556f21a0d98459e8d2e9e
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2025-06-30 03:32:33 +0700

    feat: update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
-import { Header } from "./components/layout/header"
-import { Hero } from "./components/sections/hero"
-import { FeaturedEvents } from "./components/sections/featured-events"
-import { EventCategories } from "./components/sections/event-categories"
-import { Footer } from "./components/layout/footer"
+import DashboardPage from "./pages/dashboard/page"
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen">
-      <Header />
-      <main>
-        <Hero />
-        <EventCategories />
-        <FeaturedEvents />
-      </main>
-      <Footer />
-    </div>
+    <DashboardPage />
   )
 }
