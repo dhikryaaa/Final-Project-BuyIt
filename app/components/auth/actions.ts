375933d6fe3cf0db6b351cb8d1db57baf6633023
Andhika Aryaputra <andhika.aryaputra@gmail.com>
2025-06-29 12:48:09 +0700

    feat: add styling to login and register pages

--- app/components/auth/actions.ts
+++ app/components/auth/actions.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
-import { createClient } from '../../../../supabase/server'
+import { createClient } from '../../../supabase/server'
 
 export async function login(formData: FormData) {
   const supabase = await createClient()
@@ -22,7 +22,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/')
+  redirect('/pages/dashboard')
 }
 
 export async function signup(formData: FormData) {
@@ -42,5 +42,5 @@ export async function signup(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  redirect('/')
+  redirect('/pages/auth/login')
 }
